1a3411fe07171b5e1f54f2363f201b4c3c2e78c7
Vadim Markov <vadim_markov@epam.com>
2022-06-30 01:15:06 +0200

    Initial

---
+++ src/main/java/edu/epam/fop/lambdas/CompositeAreasChecker.java
@@ -0,0 +1,18 @@
+package edu.epam.fop.lambdas;
+
+import java.util.function.Predicate;
+
+public interface CompositeAreasChecker {
+
+  static Predicate<Point> task1() {
+    return null;
+  }
+
+  static Predicate<Point> task2() {
+    return null;
+  }
+
+  static Predicate<Point> task3() {
+    return null;
+  }
+}
